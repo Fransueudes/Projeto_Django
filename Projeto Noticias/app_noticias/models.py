b79b7cf2e26f065ea45ce2893a0448b2f2ddb111
Fransueudes Alexandre Freitas <fransueudes@rede.ulbra.br>
2020-02-21 21:44:17 -0300

    Projeto Django

---
+++ Projeto Noticias/app_noticias/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+class Noticia(models.Model):
+    titulo = models.CharField('titulo' , max_length=128, blank=True, null=True)
+    conteudo = models.TextField()
+    autor = models.CharField('autor', max_length=128, blank=True, null=True)
+
+    def __str__(self):
+        return self.titulo
+    
+    class Meta:
+        verbose_name = 'Notícia'
+        verbose_name_plural = 'Notícias'
+
+      
+    
+
+# Create your models here.
